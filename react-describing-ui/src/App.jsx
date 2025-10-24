ba82abb9ff550fcf88051e00951dcf7b1ca99439
rolandomajait1-rgb <rolandomajait1@gmail.com>
2025-10-25 00:15:38 +0800

    Done with when and how to create multi-component files

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,21 +1,17 @@
-import './index.css';
-
-function Profile() {
+export default function TodoList() {
   return (
-    <img
-      src="https://i.imgur.com/MK3eW3As.jpg"
-      alt="Katherine Johnson"
-    />
-  );
-}
-
-export default function Gallery() {
-  return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve spectrum technology</li>
+      </ul>
+    </>
   );
 }
---
+++ react-describing-ui/src/components/Gallery.jsx
@@ -0,0 +1,12 @@
+import Profile from './Profile';
+
+export default function Gallery() {
+  return (
+    <section>
+      <h2>Amazing Photographers</h2>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
