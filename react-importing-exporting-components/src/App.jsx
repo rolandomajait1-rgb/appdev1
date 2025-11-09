a0d22ef9799142e26ae76e7922a0b23e14f76aa2
rolandomajait1-rgb <rolandomajait1@gmail.com>
2025-11-09 21:17:04 +0800

    Done with exporting and importing a component

--- react-importing-exporting-components/src/App.jsx
+++ react-importing-exporting-components/src/App.jsx
@@ -1,10 +1,14 @@
-
+import Profile from './Components/Profile.jsx'
 import './App.css'
 
 export default function App() {
   return (
     <div>
       <h1>Importing and Exporting Components</h1>
+      <h2>Section: Default Export/Import</h2>
+      <Profile />
+      <Profile />
+      <Profile />
     </div>
   );
 }
