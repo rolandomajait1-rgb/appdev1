8e471ad6a42e74234ec2e24f1b822918db067e36
rolandomajait1-rgb <rolandomajait1@gmail.com>
2025-11-09 18:10:28 +0800

    Done with defining a component

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+// 1. DEFINING A COMPONENT
+// This is the component definition, as per the slides.
+function Profile() {
+  return (
+    <h2>I am a new Profile Component!</h2>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>My React App</h1>
+      {/* We will USE the component in the next step */}
+    </div>
+  );
+}
+
+export default App;
