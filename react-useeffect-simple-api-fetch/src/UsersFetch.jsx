d4ee39c5d85e29ead17eb7cf605bdc33f4b00851
rolandomajait1-rgb <rolandomajait1@gmail.com>
2025-11-23 19:35:42 +0800

    Done with fetching user data exercise

---
+++ react-useeffect-simple-api-fetch/src/UsersFetch.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+export default function UsersFetch() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+  
+    fetch("https://jsonplaceholder.typicode.com/users?_limit=5")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []); 
+
+  if (loading) return <p>Loading users...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div>
+      <h2>Users List (Simple Fetch)</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <strong>{user.name}</strong> - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
